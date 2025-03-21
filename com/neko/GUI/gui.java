d144e90551164f29d507e6ae035243328f25d209
Histrem Rakik <99650653+lsjllxhc@users.noreply.github.com>
2025-03-22 06:52:44 +0800

    更新 gui.java

--- com/neko/GUI/gui.java
+++ com/neko/GUI/gui.java
@@ -3,4 +3,15 @@ package com.neko.GUI;
 //引入awt中的所有库
 import java.awt.*;
 
-Frame(String UnLocker-GUI);
+public class GUI {
+    public static void main(String[] args) {
+        // 创建一个 Frame 对象，并设置标题
+        Frame frame = new Frame("UnLocker-GUI");
+
+        // 设置 Frame 的大小
+        frame.setSize(400, 300);
+
+        // 设置 Frame 可见
+        frame.setVisible(true);
+    }
+}
