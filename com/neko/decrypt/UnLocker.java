4683d3eb0b81cfe0190c633cbbdf49d62e88b05d
Histrem Rakik <99650653+lsjllxhc@users.noreply.github.com>
2025-03-21 19:37:06 +0800

    Create UnLocker.java

---
+++ com/neko/decrypt/UnLocker.java
@@ -0,0 +1,147 @@
+package com.neko.decrypt;
+
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.FileVisitor;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.stream.Stream;
+
+import static com.neko.decrypt.MMK.*;
+
+public class UnLocker {
+    public static void main(String[] args) throws Exception {
+        Path srcPath = Path.of("G:\\原始");
+        Path outPath = Path.of("G:\\out");
+
+
+        try (Stream<Path> lines = Files.list(srcPath)) {
+            var iterator = lines.iterator();
+            while (iterator.hasNext()) {
+                Path path = iterator.next();
+                try {
+                    SecretKey secretKey = getSecretKey(path);
+                    if (secretKey == null)
+                        continue;
+                    System.out.println("正在处理: " + path + " " + secretKey);
+                    handleDiv(path, outPath, secretKey);
+                } catch (Exception e) {
+                    System.out.println("Error: " + path + " -> " + e.getMessage());
+                }
+            }
+        }
+    }
+
+    /**
+     * 处理目录
+     *
+     * @param srcDiv    源目录
+     * @param targetDiv 目标目录
+     * @param secretKey 密钥
+     */
+    public static void handleDiv(Path srcDiv, Path targetDiv, SecretKey secretKey) throws IOException {
+        Files.walkFileTree(srcDiv, new FileVisitor<>() {
+            @Override
+            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                Path relativize = srcDiv.getParent().relativize(dir);
+                Path target = targetDiv.resolve(relativize);
+                if (!Files.exists(target))
+                    Files.createDirectory(target);
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                Path relativize = srcDiv.getParent().relativize(file);
+                Path target = targetDiv.resolve(relativize);
+
+                if (!Files.exists(target))
+                    switch (file.getFileName().toString()) {
+                        case "fileMap.json", "meta.mko" -> {
+                            Files.copy(file, target);
+                        }
+                        default -> {
+                            byte[] data = secretKey.aes(Files.readAllBytes(file));
+                            if (data == null) {
+                                System.out.println("Warn: " + file + " 无法解密此文件");
+                                break;
+                            }
+                            Files.write(target, data);
+                        }
+                    }
+
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                return FileVisitResult.CONTINUE;
+            }
+        });
+    }
+
+    /**
+     * 通过寻找json文件来尝试各个密钥
+     *
+     * @param rootPath 根目录
+     * @return 最终密钥
+     */
+    public static MMK.SecretKey getSecretKey(Path rootPath) throws IOException {
+        final Path[] files = new Path[3];
+        Files.walkFileTree(rootPath, new FileVisitor<>() {
+            @Override
+            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                if (file.toString().endsWith(".json")) {
+                    if (isJson(Files.readAllBytes(file)))
+                        return FileVisitResult.CONTINUE;
+                    files[0] = file;
+                    return FileVisitResult.TERMINATE;
+                }
+                if (file.toString().endsWith(".png") || file.toString().endsWith(".jpg")) {
+                    if (isImage(Files.readAllBytes(file)))
+                        return FileVisitResult.CONTINUE;
+                    files[1] = file;
+                    return FileVisitResult.TERMINATE;
+                }
+                if (file.toString().endsWith(".mp4")) {
+                    if (isMp4(Files.readAllBytes(file)))
+                        return FileVisitResult.CONTINUE;
+                    files[2] = file;
+                    return FileVisitResult.TERMINATE;
+                }
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                return FileVisitResult.CONTINUE;
+            }
+        });
+
+        if (files[0] != null) {
+            return MMK.SecretKey.getFromJson(Files.readAllBytes(files[0]));
+        } else if (files[1] != null) {
+            return MMK.SecretKey.getFromImage(Files.readAllBytes(files[1]));
+        } else if (files[2] != null) {
+            return MMK.SecretKey.getFromMp4(Files.readAllBytes(files[2]));
+        } else {
+            throw new RuntimeException("无法判断");
+        }
+    }
+}
