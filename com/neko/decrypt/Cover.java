59ef3ea452f27992f56801cb976efd2873352caa
Histrem Rakik <99650653+lsjllxhc@users.noreply.github.com>
2025-03-24 18:52:46 +0800

    更新 Cover.java

--- com/neko/decrypt/Cover.java
+++ com/neko/decrypt/Cover.java
@@ -1,7 +1,19 @@
 package com.neko.decrypt;
 
 public class Cover{
-    public static void coverDir(PATH srcPath, PATH outPath){
-    
+    public static void coverDir(Path srcPath, Path outPath) throws IOException{
+        if (Files.exists(srcPath)) {
+            try (Stream<Path> walk = Files.walk(srcPath)) {
+                walk.sorted(Comparator.reverseOrder())
+                    .forEach(path -> {
+                        try {
+                            Files.delete(path);
+                        } 
+                        catch (IOException e) {
+                            throw new RuntimeException("Failed to delete " + path, e);
+                        }
+                    });
+            }
+        }
     }
 }
