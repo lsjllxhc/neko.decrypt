c56d175a5d433ec39cd1ba3000ff0d79663ef9c2
Histrem Rakik <99650653+lsjllxhc@users.noreply.github.com>
2025-03-24 19:18:03 +0800

    更新 Cover.java

--- com/neko/decrypt/Cover.java
+++ com/neko/decrypt/Cover.java
@@ -1,34 +1,57 @@
 package com.neko.decrypt;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Comparator;
-import java.util.stream.Stream;
+import java.nio.file.Paths;
 
-public class Cover{
-    public static void coverDir(Path srcPath, Path outPath) throws IOException{
-        if (Files.exists(srcPath)) {
-            try (Stream<Path> walk = Files.walk(srcPath)) {
-                walk.sorted (Comparator.reverseOrder())
-                    .forEach(path -> {
-                        try {
-                            Files.delete(path);
-                        } 
-                        catch (IOException e) {
-                            throw new RuntimeException("Failed to delete " + path, e);
-                        }
-                    });
-            }
+public class FileMover {
+
+    public static void coverDir(Path srcPath, Path outPath) {
+        try {
+            moveFiles(srcPath, outPath);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        if (Files.exists(outPath)) {
-            try (Stream<Path> walk = Files.walk(outPath)) {
-                walk.sorted（Comparator.naturalOrder()).forEach(path -> {
+    }
+
+    public static void moveFiles(Path srcPath, Path outPath) throws IOException {
+        // 删除srcPath内的所有文件与非空文件夹
+        deleteDirectoryContents(srcPath);
+
+        // 将outPath文件夹内所有文件与非空文件夹移到srcPath内
+        moveDirectoryContents(outPath, srcPath);
+
+        // 删除outPath文件夹
+        Files.delete(outPath);
+    }
+
+    private static void deleteDirectoryContents(Path path) throws IOException {
+        if (Files.exists(path)) {
+            Files.walk(path)
+                .sorted((a, b) -> b.compareTo(a)) // 先删除子文件/文件夹
+                .forEach(p -> {
                     try {
-                        if (Files.isDiretcory(path)) {
-                            Files.createDirectory(path);
-                        }
-                        else {
-                        
+                        Files.delete(p);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });
+        }
+    }
+
+    private static void moveDirectoryContents(Path srcDir, Path destDir) throws IOException {
+        if (Files.exists(srcDir)) {
+            Files.walk(srcDir)
+                .forEach(src -> {
+                    Path dest = destDir.resolve(srcDir.relativize(src));
+                    try {
+                        Files.move(src, dest);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });
+        }
     }
 }
