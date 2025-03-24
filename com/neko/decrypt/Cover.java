db3a76fb3670b94b1fc96ceb52db42f0140e5e8f
Histrem Rakik <99650653+lsjllxhc@users.noreply.github.com>
2025-03-24 20:10:43 +0800

    更新 Cover.java

--- com/neko/decrypt/Cover.java
+++ com/neko/decrypt/Cover.java
@@ -8,51 +8,6 @@ import java.util.Comparator;
 public class Cover {
 
     public static void coverDir(Path srcPath, Path outPath) {
-        try {
-            moveFiles(srcPath, outPath);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static void moveFiles(Path srcPath, Path outPath) throws IOException {
-        // 删除srcPath内的所有文件与非空文件夹
-        deleteDirectoryContents(srcPath);
-
-        // 将outPath文件夹内所有文件与非空文件夹移到srcPath内
-        moveDirectoryContents(outPath, srcPath);
-
-        // 删除outPath文件夹
-        if (Files.exists(outPath)) {
-            Files.delete(outPath);
-        }
-    }
-
-    private static void deleteDirectoryContents(Path path) throws IOException {
-        if (Files.exists(path)) {
-            Files.walk(path)
-                    .sorted(Comparator.reverseOrder()) // 先删除子文件/文件夹
-                    .forEach(p -> {
-                        try {
-                            Files.delete(p);
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    });
-        }
-    }
-
-    private static void moveDirectoryContents(Path srcDir, Path destDir) throws IOException {
-        if (Files.exists(srcDir)) {
-            Files.walk(srcDir)
-                    .forEach(src -> {
-                        Path dest = destDir.resolve(srcDir.relativize(src));
-                        try {
-                            Files.move(src, dest);
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    });
-        }
+        
     }
 }
