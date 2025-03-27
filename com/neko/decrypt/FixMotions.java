30db81cff67bd3fb5f31a473ba00dcfbeae45e15
Histrem Rakik <99650653+lsjllxhc@users.noreply.github.com>
2025-03-27 13:10:52 +0800

    更新 FixMotions.java

--- com/neko/decrypt/FixMotions.java
+++ com/neko/decrypt/FixMotions.java
@@ -1,4 +1,4 @@
-package codm.neko.decrypt
+package codm.neko.decrypt;
 
 import java.io.File;
 import java.io.FileReader;
@@ -6,7 +6,6 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Scanner;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -17,17 +16,11 @@ public class FixMotions {
     public static void FIX(String path) {
         // 获取文件夹目录路径
         String folderPath = path;
-        
+
         File folder = new File(folderPath);
         processFolder(folder);
-
-        // 等待用户按下任意键退出
-        System.out.print("按下任意键退出...");
-        Scanner scanner = new Scanner(System.in);
-        scanner.nextLine();
-        scanner.close();
     }
-    
+
     private static void processFolder(File folder) {
         // 遍历文件夹中的所有文件
         File[] files = folder.listFiles();
@@ -43,14 +36,14 @@ public class FixMotions {
             }
         }
     }
-    
+
     private static void processFile(File file) {
         try (FileReader reader = new FileReader(file);
              FileWriter writer = new FileWriter(file)) {
             // 读取JSON文件
             JSONTokener tokener = new JSONTokener(reader);
             JSONObject data = new JSONObject(tokener);
-            
+
             // 查找id为LipSync的segment
             JSONArray curves = data.getJSONArray("Curves");
             JSONArray lipsyncSegment = null;
@@ -61,7 +54,7 @@ public class FixMotions {
                     break;
                 }
             }
-            
+
             // 替换id为MouseOpenY的segment
             for (int i = 0; i < curves.length(); i++) {
                 JSONObject curve = curves.getJSONObject(i);
@@ -70,7 +63,7 @@ public class FixMotions {
                     break;
                 }
             }
-            
+
             // 删除id为LipSync的数据组
             Iterator<Object> iterator = curves.iterator();
             while (iterator.hasNext()) {
@@ -79,10 +72,10 @@ public class FixMotions {
                     iterator.remove();
                 }
             }
-            
+
             // 写回修改后的JSON文件
             writer.write(data.toString(4));
-            
+
             System.out.println("Segments replaced and LipSync data group deleted successfully in " + file.getName() + ".");
         } catch (IOException e) {
             e.printStackTrace();
